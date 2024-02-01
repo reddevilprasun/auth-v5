2d1b876f5644de4c9712fedbd410c1a42361f0b7
PrasunKamilya <prasunkamilya26@gmail.com>
2024-02-02 03:11:26 +0530

    modified:   actions/settings.ts

--- actions/settings.ts
+++ actions/settings.ts
@@ -3,9 +3,11 @@
 import * as z from "zod"
 import { db } from "@/lib/db";
 import { SettingsSchema } from "@/schemas";
-import { getUserById } from "@/data/user";
+import { getUserByEmail, getUserById } from "@/data/user";
 import { currentUser } from "@/lib/auth";
-
+import { genarateVerificationToken } from "@/lib/token";
+import { sendVarificationEmail } from "@/lib/mail";
+import bcrypt from "bcryptjs";
 export const settings = async (
     values: z.infer<typeof SettingsSchema>
 ) => {
@@ -23,6 +25,29 @@ export const settings = async (
         values.password = undefined;
         values.newPassword = undefined;
         values.isTwoFactorEnabled = undefined;
+    }
+
+    if(values.email && values.email !== user.email){
+        const existingUser = await getUserByEmail(values.email);
+        if(existingUser && existingUser.id !== user.id){
+            return{error :"This email already in use!"};
+        }
+        const verificationToken = await genarateVerificationToken(values.email);
+        await sendVarificationEmail(
+            verificationToken.email,
+            verificationToken.token,
+        );
+        return {success: "Verification email send!"}    
+    }
+
+    if(values.password && values.newPassword && dbUser.password){
+        const passwordMatch =  await bcrypt.compare(values.password,dbUser.password);
+        if(!passwordMatch){
+            return {error: "Incorrect password!"}
+        }
+        const hashedPassword = await  bcrypt.hash(values.newPassword,10);
+        values.password = hashedPassword;
+        values.newPassword = undefined;
     }
 
     await db.user.update({
