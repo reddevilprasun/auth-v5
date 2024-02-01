0ff8a7ef6d4eef5d92953e1fdc92dca9e785ce13
PrasunKamilya <prasunkamilya26@gmail.com>
2024-02-01 23:36:35 +0530

    new file:   actions/admin.ts 	new file:   app/(protected)/admin/page.tsx 	new file:   app/api/admin/route.ts 	modified:   app/layout.tsx 	new file:   components/auth/role-gate.tsx 	new file:   components/ui/sonner.tsx 	new file:   hooks/use-current-role.ts 	modified:   lib/auth.ts 	modified:   package-lock.json 	modified:   package.json

---
+++ components/auth/role-gate.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { useCurrentRole } from "@/hooks/use-current-role";
+import { UserRole } from "@prisma/client";
+import { FromError } from "../form-error";
+interface RoleGateProps{
+    children: React.ReactNode;
+    allowedRole: UserRole;
+};
+
+export  const RoleGate = ({
+    children,
+    allowedRole,
+}:RoleGateProps) => {
+    const role= useCurrentRole();
+    if(role !== allowedRole){
+        return (
+            <FromError message="You do not permission to view this content!"/>
+        )
+    }
+    return <>{children}</>;
+}             
